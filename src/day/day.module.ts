4538b74a639ae1b456cb7f999ca9488d573d8e1f
ahmad baghereslami <ahmad.b1995@gmail.com>
2024-05-21 14:25:53 +0330

    minor

--- src/day/day.module.ts
+++ src/day/day.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DayService } from './day.service';
 import { DayController } from './day.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from 'src/tasks/entities/task.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Task])],
   controllers: [DayController],
   providers: [DayService],
 })
